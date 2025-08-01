7f858576d830f0196714fddfcea9d49f938dcbc2
digitalscorpyun <mikerkibbe73@gmail.com>
2025-08-01 17:18:53 -0400

    saved emtion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,11 +3,8 @@ import requests
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = {"raw_document": {"text": text_to_analyze}}
+    input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=input_json)
-    result = response.json()
-    # Ensure the key exists before accessing
-    if 'document' in result and 'emotion' in result['document']:
-        return result['document']['emotion']
-    return {}
+    data = response.json()
+    return data.get('emotionPredictions', [{}])[0].get('emotion', {})
